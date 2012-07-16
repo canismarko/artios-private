dc0a0cddce100d09b942ce29b105c415fb72fc66
Mark Wolf <mark.wolf.music@gmail.com>
2012-07-16 18:54:02 -0400

    Switched some links to buttons and added search functions. Note these functions are not utilized yet, merely copy/pasted from a website: http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap

--- songs/views.py
+++ songs/views.py
@@ -1,12 +1,17 @@
+import re
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from artios_privatesite.songs.models import Song
 from artios_privatesite.songs.forms import *
+from django.db.models import Q
 
 # List the songs in a table view
 def list(request):
-    song_list = Song.objects.all()
+    # TODO: Add ability to filter and search.
+    #   search functions are already written
+    #   below but might need modification.
+    song_list = Song.objects.order_by('order')
     return render_to_response('song_list.html',
                               locals(), 
                               RequestContext(request))
@@ -75,3 +80,39 @@ def edit(request, song_id=None, function="standard"):
     return render_to_response('song_details.html',
                               locals(),
                               RequestContext(request))
+
+# Return a normalized search query
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+        and grouping quoted words together.
+        Example:
+        
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    
+    '''
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+
+# Returns a query, to be used with Model.get()
+def get_query(query_string, search_fields):
+    ''' Returns a query, that is a combination of Q objects. That combination
+        aims to search keywords within a model by testing the given search fields.
+    
+    '''
+    query = None # Query to search for every search term        
+    terms = normalize_query(query_string)
+    for term in terms:
+        or_query = None # Query to search for a given term in each field
+        for field_name in search_fields:
+            q = Q(**{"%s__icontains" % field_name: term})
+            if or_query is None:
+                or_query = q
+            else:
+                or_query = or_query | q
+        if query is None:
+            query = or_query
+        else:
+            query = query & or_query
+    return query
